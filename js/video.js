b577f4705f3803012d1ec557bce6824f72727922
Unknown <jasonscholefield@gmail.com>
2017-05-06 02:06:07 -0700

    Class list

---
+++ js/video.js
@@ -0,0 +1,11 @@
+var figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+	$('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+	$('video', this).get(0).pause(); 
+}
+
+document.querySelector('video').playbackRate = 1;
