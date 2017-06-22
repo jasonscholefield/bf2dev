b5e29a2038319d55883affd5b71d816ec635d424
Jason <jason.scholefield@laneterralever.com>
2017-06-21 17:32:22 -0700

    footer & vehicle row updates

--- js/vehicles.js
+++ js/vehicles.js
@@ -1,54 +1,40 @@
 var tag = document.createElement('script');
-    tag.src = 'https://www.youtube.com/player_api';
+		tag.src = 'https://www.youtube.com/player_api';
 var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var tv,
-    playerDefaults = {autoplay: 0, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, disablekb: 1, enablejsapi: 0, iv_load_policy: 3};
+		playerDefaults = {autoplay: 0, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, disablekb: 1, enablejsapi: 0, iv_load_policy: 3};
 var vid = [
-      {'videoId': '09w9MTtZDEM', 'startSeconds': 1, 'endSeconds': 10, 'suggestedQuality': 'hd720'}, // Fury
-      {'videoId': '09w9MTtZDEM', 'startSeconds': 71, 'endSeconds': 77, 'suggestedQuality': 'hd720'},
-      {'videoId': '09w9MTtZDEM', 'startSeconds': 94, 'endSeconds': 98, 'suggestedQuality': 'hd720'},
-      {'videoId': '09w9MTtZDEM', 'startSeconds': 100, 'endSeconds': 103, 'suggestedQuality': 'hd720'},
-      {'videoId': 'T7O7BtBnsG4', 'startSeconds': 33, 'endSeconds': 37, 'suggestedQuality': 'hd720'}, // Dunkirk
-      {'videoId': 'T7O7BtBnsG4', 'startSeconds': 55, 'endSeconds': 65, 'suggestedQuality': 'hd720'},
-      {'videoId': 'T7O7BtBnsG4', 'startSeconds': 95, 'endSeconds': 98, 'suggestedQuality': 'hd720'},
-      {'videoId': 'T7O7BtBnsG4', 'startSeconds': 134, 'endSeconds': 142, 'suggestedQuality': 'hd720'}
-    ],
-    randomVid = Math.floor(Math.random() * vid.length),
-    currVid = randomVid;
+  //insert Youtube ID here
+			{'videoId': '7UAqXYfh46k', 'startSeconds': 0, 'endSeconds': 65, 'suggestedQuality': 'hd720'}
+		],
+		randomvid = Math.floor(Math.random() * (vid.length - 1 + 1));
 
 function onYouTubePlayerAPIReady(){
   tv = new YT.Player('tv', {events: {'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange}, playerVars: playerDefaults});
 }
 
 function onPlayerReady(){
-  tv.loadVideoById(vid[currVid]);
+  tv.loadVideoById(vid[randomvid]);
   tv.mute();
 }
 
 function onPlayerStateChange(e) {
   if (e.data === 1){
     $('#tv').addClass('active');
-  } else if (e.data === 2){
-    $('#tv').removeClass('active');
-    if(currVid === vid.length - 1){
-      currVid = 0;
-    } else {
-      currVid++;  
-    }
-    tv.loadVideoById(vid[currVid]);
-    tv.seekTo(vid[currVid].startSeconds);
+  } else if (e.data === 0){
+    tv.seekTo(vid[randomvid].startSeconds)
   }
 }
 
 function vidRescale(){
 
-  var w = $(window).width()+200,
-    h = $(window).height()+200;
+  var w = $(window).width(),
+    h = $(window).height();
 
   if (w/h > 16/9){
     tv.setSize(w, w/16*9);
-    $('.tv .screen').css({'left': '0px'});
+    $('.tv .screen').css({'left': '50%'});
   } else {
     tv.setSize(h/9*16, h);
     $('.tv .screen').css({'left': -($('.tv .screen').outerWidth()-w)/2});
